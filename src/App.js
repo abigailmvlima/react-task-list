e70d4ec5636cde74af4c97b966031db48bcf1df1
abigail lima <abigailmvlima@outlook.com>
2021-12-25 20:38:43 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+import Tasks from './componets/Tasks';
+import './App.css';
+
+const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+      id: "1",
+      title: "Estudar Programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler Livros",
+      completed: "true",
+    },
+  ]);
+
+   return (
+     <div>
+      <div className="container">
+         <Tasks tasks={tasks}/>
+      </div>
+     
+    </div>
+    );
+   };
+
+   export default App;
